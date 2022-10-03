7a603570b45fc10a8e4e740d3277e5cf85f1e645
jasonlq007 <jasonlq007@gmail.com>
2022-10-03 11:02:03 +0200

    Initial commit

---
+++ fish_wiki/server.js
@@ -0,0 +1,65 @@
+/**
+ *
+ * https://www.digitalocean.com/community/tutorials/how-to-implement-caching-in-node-js-using-redis
+ * https://redis.io/docs/getting-started/installation/install-redis-on-windows/
+ * @type {e | (() => Express)}
+ */
+
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
+const app = express();
+const port = process.env.PORT || 3000;
+
+let redisClient;
+
+(async () => {
+    redisClient = redis.createClient();
+
+    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+    await redisClient.connect();
+})();
+
+async function fetchApiData(species) {
+    const apiResponse = await axios.get(
+        `https://www.fishwatch.gov/api/species/${species}`
+    );
+    console.log("Request sent to the API");
+    return apiResponse.data;
+}
+
+
+async function getSpeciesData(req, res) {
+    const species = req.params.species;
+    let results;
+    let isCached = false;
+
+    try {
+        const cacheResults = await redisClient.get(species);
+        if (cacheResults) {
+            isCached = true;
+            results = JSON.parse(cacheResults);
+        } else {
+            results = await fetchApiData(species);
+            if (results.length === 0) {
+                throw "API returned an empty array";
+            }
+            await redisClient.set(species, JSON.stringify(results));
+
+        }
+        res.send({
+            fromCache: isCached,
+            data: results,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(404).send("Data unavailable");
+    }
+}
+
+app.get("/fish/:species", getSpeciesData);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
